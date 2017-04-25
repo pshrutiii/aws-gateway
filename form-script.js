57d9b6ef7e60b9c9f70483b9dc6f85f203447a33
pshrutiii <contactdevshruti@gmail.com>
2017-04-25 13:23:48 -0700

    base form ready

---
+++ form-script.js
@@ -0,0 +1,49 @@
+function calc_total(price){
+    var sum = 0;
+    $('.input-amount').each(function(){
+        sum += parseFloat($(this).text());
+    });
+    $(".preview-total").text(sum.toFixed(2));    
+}
+
+function get_price(size){
+    var price = 0;
+    if (size==="Short"){
+        price = 1.85;
+    }else if(size === "Tall"){
+        price = 2.95;
+    }else if(size === "Grande"){
+        price = 3.65;
+    }else{
+        price = 4.15;
+    }
+    return price;
+}
+
+$(document).on('click', '.input-remove-row', function(){ 
+    var tr = $(this).closest('tr');
+    tr.fadeOut(200, function(){
+        tr.remove();
+        calc_total()
+    });
+});
+
+$(function(){
+    $('.preview-add-button').click(function(){
+        var form_data = {};
+        form_data["quantity"] = parseFloat($('.payment-form input[name="quantity"]').val());
+        form_data["name"] = $('.payment-form #name option:selected').text();
+        form_data["milk"] = $('.payment-form #milk option:selected').text();
+        form_data["size"] = $('.payment-form #size option:selected').text();
+        form_data["location"] = $('.payment-form #location option:selected').text();
+        form_data["amount"] = parseFloat(form_data["quantity"] * get_price($('.payment-form #size option:selected').text())).toFixed(2);
+        form_data["remove-row"] = '<span class="glyphicon glyphicon-remove"></span>';
+        var row = $('<tr></tr>');
+        $.each(form_data, function( type, value ) {
+            $('<td class="input-'+type+'"></td>').html(value).appendTo(row);
+        });
+        $('.preview-table > tbody:last').append(row); 
+        calc_total();
+        
+    });  
+});
--- index.php
+++ index.php
@@ -1,36 +1,134 @@
 <!DOCTYPE html>
-<?php echo "hello PHP"?>
 <html>
 <head>
-<title>Starbucks Page</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel='shortcut icon' href='fav.png' type='image/x-icon'/ >
+    <title>Starbucks Page</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="/resources/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="/resources/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 </head>
 <body>
+<div class="container">
+  <div class="row">
+        <div class="col-sm-12">
+            <img src="logo.png" href="/" style="width:120px;"/>
+        </div>
+        <div style="padding-top: 15%;"></div>
+        <!-- panel preview -->
+        <div class="col-sm-5">
+            <h4>Place Order</h4>
+            <div class="panel panel-default">
+                <div class="panel-body form-horizontal payment-form">
+                    <div class="form-group">
+                        <label for="location" class="col-sm-3 control-label">Location</label>
+                        <div class="col-sm-9">
+                            <select class="form-control" id="location" name="location">
+                                <option>San Francisco</option>
+                                <option>Los Angeles</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="quantity" class="col-sm-3 control-label">Quantity</label>
+                        <div class="col-sm-9">
+                            <input type="number" min="1" class="form-control" id="quantity" name="quantity" value="1">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="milk" class="col-sm-3 control-label">Milk</label>
+                        <div class="col-sm-9">
+                            <select class="form-control" id="milk" name="milk">
+                                <option>--Choose One--</option>
+                                <option>Coconut Milk</option>
+                                <option>Soy Milk</option>
+                                <option>Half N Half</option>
+                            </select>
+                        </div>
+                    </div>  
+                    <div class="form-group">
+                        <label for="name" class="col-sm-3 control-label">Name</label>
+                        <div class="col-sm-9">
+                            <select class="form-control" id="name" name="name">
+                                <option>--Choose One--</option>
+                                <option>Latte Macchiato</option>
+                                <option>Americano</option>
+                                <option>Expresso Macchiato</option>
+                                <option>Cappuccino</option>
+                                <option>Latte</option>
+                                <option>Caramel Macchiato</option>
+                                <option>Mocha</option>
+                            </select>
+                        </div>
+                    </div> 
+                    <div class="form-group">
+                        <label for="size" class="col-sm-3 control-label">Size</label>
+                        <div class="col-sm-9">
+                            <select class="form-control" id="size" name="size">
+                                <option>--Choose One--</option>
+                                <option>Short</option>
+                                <option>Tall</option>
+                                <option>Grande</option>
+                                <option>Venti</option>
+                            </select>
+                        </div>
+                    </div>          
+                    <div class="form-group">
+                        <div class="col-sm-12 text-right">
+                            <button type="button" class="btn btn-default preview-add-button">
+                                <span class="glyphicon glyphicon-plus"></span> Add
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>            
+        </div> <!-- / panel preview -->
+        <div class="col-sm-7">
+            <h4>Preview:</h4>
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="table-responsive">
+                        <table class="table preview-table">
+                            <thead>
+                                <tr>
+                                    <th>Qty</th>
+                                    <th>Name</th>
+                                    <th>Milk</th>
+                                    <th>Size</th>
+                                    <th>Pickup Location</th>
+                                    <th>Amount</th>
+                                </tr>
+                            </thead>
+                            <tbody></tbody> <!-- preview content goes here-->
+                        </table>
+                    </div>                            
+                </div>
+            </div>
+            <div class="row text-right">
+                <div class="col-xs-12">
+                    <h4>Total: $<strong><span class="preview-total"></span></strong></h4>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12">
+                    <hr style="border:1px dashed #dddddd;">
+                    <button type="button" class="btn btn-block" style="background: #006341; color: white; font-size: 15px; font-weight: 600;">Pay Now</button>
+                </div>                
+            </div>
+        </div>
+  </div>
+</div>
 
-<h1>Testing page</h1>
-<p>Trial paragraph</p>
- 
-<form method="post" action="" name="contact_form" id="contact_form" >
-<div class="row">
-			<div class="label"><span style="color:#F00;">*</span>Name: </div>
-			<div class="input-row" ><input name="your_name" id="your_name" type="text" class="textbox" autocomplete="off"><span id="your_nameErr" class="error" ></span></div>
-		</div>
- 
-<div class="row">
-			<div class="label"><span style="color:#F00;">*</span>Email: </div>
-			<div class="input-row" ><input name="your_email" id="your_email" type="text" class="textbox" autocomplete="off"><span id="your_emailErr" class="error" ></span></div>
- 
-		</div>
- 
-<div class="row">
-			<div class="label"></div>
-			<div class="dummy-container" ><input type="submit" name="sub" value="Send" > <span class="loading"></span> </div>
-		</div>
- 
-<!--Form final confirmation of form submission-->
-<div class="output"></div>
- 
-</form>
-
-
+  <script src="../resources/jquery/jquery.min.js"></script> <!-- jQuery -->
+  <script src="../resources/bootstrap/js/bootstrap.min.js"></script> <!-- Bootstrap Core JavaScript -->
+  <script src="form-script.js"></script>
+  
 </body>
 </html>
